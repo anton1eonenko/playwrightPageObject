89828ed186be69369f381570b0beb3aec55858e9
Anton Leonenko <UI557266@rwe-ag.com>
2023-07-24 14:46:16 +0300

    deleted waiter

--- src/tests/searchByMark.spec.ts
+++ src/tests/searchByMark.spec.ts
@@ -6,28 +6,25 @@ import Log from '../utils/logger.utils'
 test.only('Seach By Mark', async ({ app }) => {
   Log.step('1. Открываем страницу'); 
   await app.base.openAvBy();
-
-  Log.step("2. Ожидаем...");
-  await app.searchByParameters.waiter();
-
-  Log.step('3. Нажимаем на поле "Марка"');
+  
+  Log.step('2. Нажимаем на поле "Марка"');
   await app.searchByParameters.clickOnMarkButton(); 
   
-  Log.step("4. Вводим значение", Marks.BMW);
+  Log.step("3. Вводим значение", Marks.BMW);
   await app.searchByParameters.inputMarkName(Marks.BMW); 
 
-  Log.step("5. Ожидаем...");
+  Log.step("4. Ожидаем...");
   await app.searchByParameters.waiter(); 
 
-  Log.step("6. Нажимаем на Энтер");
+  Log.step("5. Нажимаем на Энтер");
   await app.searchByParameters.clickEnter(); 
 
-  Log.step("7. Ожидаем...");
+  Log.step("6. Ожидаем...");
   await app.searchByParameters.waiter(); 
 
-  Log.step("8. Нажимаем на кнопку 'Показать *** объявлений'");
+  Log.step("7. Нажимаем на кнопку 'Показать *** объявлений'");
   await app.searchByParameters.clickOnShowResultButton();
   
-  Log.step("9. Делаем проверку");
+  Log.step("8. Делаем проверку");
   await app.compare.searchByMarkTest(); 
 });
